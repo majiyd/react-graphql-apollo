1510e5493ffa70f7335ec38fe2602142ae07b634
Majiyd <suleimanmajiyd@gmail.com>
2019-08-09 22:46:11 +0100

    Add boilerplate code and imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import './reset.css';
 import App from './views/App'
+
+const BASE_URL = "https://api.github.com/graphql/"
+const httpLink = new HttpLink({
+  url: BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_GRAPHQL_PERSONAL_ACCESS_TOKEN}`
+  }
+})
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
