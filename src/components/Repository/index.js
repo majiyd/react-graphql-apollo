c924c15d5991546e7a227a317f20bb7be1816592
Majiyd <suleimanmajiyd@gmail.com>
2019-08-18 22:16:07 +0100

    Implement mutation

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import  gql  from "graphql-tag";
+import { Mutation } from "react-apollo";
 import Link from "../Link";
 import Star from "../Star";
 
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!){
+    addStar(input: {starrableId: $id}) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
 const Repository = props => {
-  return(
+  return (
     <div>
       <div>
         <h2>
           <Link href={props.node.url}>{props.node.name}</Link>
-          <Star 
-            numberOfStarGazers={props.node.stargazers.totalCount}
-          />
+          <Mutation 
+            mutation={STAR_REPOSITORY} 
+            variables={{id: props.node.id}}
+          >
+            {(addStar, {data, loading, error}) => (
+              <span style={{cursor: "pointer"}} onClick={addStar}>
+                <Star 
+                  numberOfStarGazers={props.node.stargazers.totalCount} 
+                />
+              </span>
+            )}
+          </Mutation>
         </h2>
       </div>
-      <div 
+      <div
         dangerouslySetInnerHTML={{
           __html: props.node.descriptionHTML
-        }} 
+        }}
       />
     </div>
-  )
+  );
 }
 
 export default Repository
