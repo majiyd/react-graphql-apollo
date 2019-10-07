0112d4ce33301aad536da0522aa28f6fe4ee90fc
Majiyd <suleimanmajiyd@gmail.com>
2019-10-07 21:21:04 +0100

    install graphql.macro

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Query} from 'react-apollo'
+import {loader} from "graphql.macro"
+
+const GET_USER_REPOSITORIES = loader('./userRepositories.graphql')
 
 const User = () => {
+  console.log(GET_USER_REPOSITORIES)
   return (
     <div>
       user maj
